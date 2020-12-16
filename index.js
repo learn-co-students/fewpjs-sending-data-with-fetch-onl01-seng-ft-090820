91071c5e17f356c3fcd8923b4eacad58e5b24099
brockbyrd <brockbyrdd@gmail.com>
2020-12-16 13:47:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email){
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }) 
+    }
+
+    return fetch("http://localhost:3000/users",  configObj)
+        .then(resp => resp.json())
+        .then(data => document.body.innerHTML = data["id"])
+        .catch(error => document.body.innerHTML = error.message)
+}
