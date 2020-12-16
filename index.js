588e0e857ebcd8292b87ae2273a008b4aa3a3352
doosemavis <moosedavis2011@gmail.com>
2020-12-16 14:43:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Add your code here
+// let data = {
+//     name: "Bob",
+//     email: "bob@bob.com"
+// };
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(data)
+//   };
+   
+function submitData(name, email) {
+    let user = {
+        name: name,
+        email: email
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(user)
+      };
+    fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(u => { 
+        document.body.innerHTML = u["id"]
+        // let body = document.querySelector('body')
+        // let li = document.createElement('li')
+        // li.innerText = u.id
+        // body.appendChild(u.id)
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+        console.log(error.message);
+    })
+}
+
+// submitData("mike", "mike@mike.com")
+// submitData("moose", "moose@moose.com")
